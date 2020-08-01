29cae775d9c69162e96797ae4289940a2a72e291
nicolasgomeztoua <nicolasnourgt@gmail.com>
2020-08-01 03:12:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var cssdisplay = document.querySelector("h3");
 var color1 = document.getElementById('color1');
 var color2 = document.getElementById('color2'); 
 var body = document.getElementById("body");
-
+var btn = document.getElementById("btn");
 
 
 
@@ -17,3 +17,22 @@ cssdisplay.textContent=body.style.background+";";}
 
 color1.addEventListener("input", changebackground); 
 color2.addEventListener("input", changebackground);
+
+
+function randomizecolors(){ 	
+	color1.value = randomColor();
+	color2.value = randomColor();
+	changebackground();
+};
+
+function randomColor() {
+	var rand = "0123456789ABCDEF";
+	var value = "#";
+	for (var i = 0; i < 6; i++) {
+		value += rand[Math.floor(Math.random() * 16)];
+	}
+	return value;
+}
+
+btn.addEventListener("click", randomizecolors);
+randomizecolors();
